0d86e9c5beaec980863aa9e4e3be329a4262816b
Qimon <simon.bergerard@gmail.com>
2021-07-14 19:25:00 +0200

    [#7] ✨ Ajout de la BDD

---
+++ app/src/main/java/fr/sber/naturecollection/PlantRepository.kt
@@ -0,0 +1,54 @@
+package fr.sber.naturecollection
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import com.google.firebase.database.*
+import fr.sber.naturecollection.PlantRepository.Singleton.databaseref
+import fr.sber.naturecollection.PlantRepository.Singleton.plantList
+
+class PlantRepository {
+
+    // L'objet Singleton permet d'éviter une réinitialisation de la liste à chaque appel de la classe PlantRepository
+    object Singleton {
+
+        // Se connecter à la ref "plants" de Firebase
+        var databaseref: DatabaseReference = FirebaseDatabase.getInstance().getReference("plants")
+
+        // Créer une liste qui récupère les éléments en BDD
+        val plantList: ArrayList<PlantModel> = arrayListOf<PlantModel>()
+    }
+
+    fun updateData(callback: () -> Unit) {
+        // Absorption des données récupérées depuis la BDD pour modifications:
+        databaseref.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // Nettoyage de la liste précédente:
+                plantList.clear()
+
+                // Récupération de la nouvelle liste des plantes déjà en BDD
+                for (ds in snapshot.children) {
+                    val plant: PlantModel? = ds.getValue(PlantModel::class.java)
+
+                    // Vérification sur la bonne récupération en BDD
+                    if (plant != null) {
+                        Log.d(TAG,"La liste n'est pas vide")
+                        plantList.add(plant)
+                    } else {
+                        Log.d(TAG,"La liste est vide")
+                    }
+                }
+                // Actionner le callback
+                callback()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e(TAG,"Erreur")
+            }
+        })
+    }
+
+    // Mettre à jour un objet plante en BDD
+    // .child pour récupérer l'enfant de la db plants
+    fun updatePlant(plant: PlantModel) = databaseref.child(plant.id).setValue(plant)
+
+}
--- app/src/main/java/fr/sber/naturecollection/adapter/PlantAdapter.kt
+++ app/src/main/java/fr/sber/naturecollection/adapter/PlantAdapter.kt
@@ -1,29 +1,72 @@
 package fr.sber.naturecollection.adapter
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import fr.sber.naturecollection.MainActivity
+import fr.sber.naturecollection.PlantModel
+import fr.sber.naturecollection.PlantRepository
 import fr.sber.naturecollection.R
 
-class PlantAdapter(private val layoutId: Int): RecyclerView.Adapter<PlantAdapter.ViewHolder>() {
+class PlantAdapter(
+    private val context: MainActivity,
+    private val plantList: List<PlantModel>,
+    private val layoutId: Int
+    ): RecyclerView.Adapter<PlantAdapter.ViewHolder>() {
 
     // Boite pour ranger tous les composants à contrôler
     class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         // Image de la plante
         val plantImage: ImageView = view.findViewById<ImageView>(R.id.image_item)
+        // le ? à la fin du type permet de prendre en compte un composant null (Spécifique Kotlin)
+        val plantName: TextView? = view.findViewById(R.id.name_item)
+        val plantDescription: TextView? = view.findViewById(R.id.description_item)
+        // Pour le like/unlike, il est présent dans les 2 viewHolder donc pas besoin de gérer les null
+        val starIcon: ImageView = view.findViewById<ImageView>(R.id.star_icon)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater
+        val view: View = LayoutInflater
             .from(parent.context)
             .inflate(layoutId, parent, false)
 
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {}
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Récupérer les Informations de la plante suivant son id dans la liste
+        val currentPlant = plantList[position]
 
-    override fun getItemCount(): Int = 5
+        // Instance du repo de la liste de plantes
+        val repo: PlantRepository = PlantRepository()
+
+        // Getter des attributs de la liste:
+        Glide.with(context).load(Uri.parse(currentPlant.imageUrl)).into(holder.plantImage)
+
+        // Mettre à jour le nom et la description de la plante:
+        holder.plantName?.text = currentPlant.name
+        holder.plantDescription?.text = currentPlant.description
+
+        // Gestion du like/unlike en fonction de l'attribut isLiked de la plante
+        if (currentPlant.isLiked) {
+            holder.starIcon.setImageResource(R.drawable.ic_like)
+        } else {
+            holder.starIcon.setImageResource(R.drawable.ic_unlike)
+        }
+
+        // Rajouter une interaction sur l'étoile
+        holder.starIcon.setOnClickListener {
+            currentPlant.isLiked = !currentPlant.isLiked
+
+            // Mettre à jour le boolean de la plante
+            repo.updatePlant(currentPlant)
+        }
+    }
+
+    override fun getItemCount(): Int = plantList.size
 }
