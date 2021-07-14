6ee814ba753c7b86c30525df9fadc9e17489422f
Qimon <simon.bergerard@gmail.com>
2021-07-14 19:25:00 +0200

    [#7] ✨ Ajout de la BDD

---
+++ app/src/main/java/fr/sber/naturecollection/PlantModel.kt
@@ -0,0 +1,9 @@
+package fr.sber.naturecollection
+
+class PlantModel(
+    val id: String = "plant0",
+    val name: String = "Tulipe",
+    val description: String = "Petite description",
+    val imageUrl: String = "http://graven.yt/plante.jpg",
+    var isLiked: Boolean = false
+)
