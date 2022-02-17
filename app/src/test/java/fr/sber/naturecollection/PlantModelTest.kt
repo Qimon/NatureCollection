4868cef16eed6f87fa41a6eaf42a99eddd298c72
Qimon37 <simon.bergerard@inetum.com>
2022-02-17 11:58:48 +0100

    ✔ - Add Unit Tests for PlantModel Class

--- app/src/test/java/fr/sber/naturecollection/PlantModelTest.kt
+++ app/src/test/java/fr/sber/naturecollection/PlantModelTest.kt
@@ -1,4 +1,52 @@
 package fr.sber.naturecollection
 
+import org.junit.Assert
+import org.junit.Test
+
 class PlantModelTest {
+
+    /**
+     * Test if PlantModel Object default attributes toString are well defined
+     *
+     */
+    @Test
+    fun toStringWithDefaultAttributesTest() {
+
+        // Prepare Expected
+        val expectedString: String = "PlantModel(id='plant0', name='Tulipe', description='Petite description', imageUrl='http://graven.yt/plante.jpg', grow='Faible', water='Moyenne', isLiked=false)"
+
+        // Run function
+        val outputString: String = PlantModel().toString()
+
+        // Assert Results
+        Assert.assertEquals(expectedString, outputString)
+    }
+
+    /**
+     * Test if PlantModel Object with overwrite attributes toString are well defined
+     *
+     */
+    @Test
+    fun toStringWithOverwriteAttributesTest() {
+
+        // Prepare Input
+        val inputPlantModel: PlantModel = PlantModel(
+            "id",
+            "name",
+            "description",
+            "imageUrl",
+            "grow",
+            "water",
+            true
+        )
+
+        // Prepare Expected
+        val expectedString: String = "PlantModel(id='id', name='name', description='description', imageUrl='imageUrl', grow='grow', water='water', isLiked=true)"
+
+        // Run function
+        val outputString: String = inputPlantModel.toString()
+
+        // Assert Results
+        Assert.assertEquals(expectedString, outputString)
+    }
 }
